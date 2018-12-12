c41760cd450d5cdf99ba38ad8daa9442dc7726b1
sgy-cd <39817547+sgy-cd@users.noreply.github.com>
2018-12-12 12:58:57 +0800

    Create publishMCSDesktop.ps1

---
+++ publishMCSDesktop.ps1
@@ -0,0 +1,61 @@
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory = $true)] [string] $ImageName,
+    [Parameter(Mandatory = $true)] [string] $GroupNameList,
+	[Parameter(Mandatory = $true)] [string] $UserList
+)
+Add-PSSnapin Citrix.*
+$idPoolName= $ImageName	
+$provSchemeName =$idPoolName
+$GroupNameArray = $GroupNameList.Split(",")
+$UserListArray = $UserList.Split(",")
+$machineCount = $GroupNameArray.Count
+$MachineArray = New-Object -TypeName System.Collections.ArrayList
+
+$LogOBJ = Start-LogHighLevelOperation  -AdminAddress $ddc -Source "Studio"  -Text "Add Computer"
+
+$adAccounts = New-AcctADAccount -IdentityPoolName $idPoolName -Count $machineCount
+
+$vms = New-ProvVm -ProvisioningSchemeName $provSchemeName -ADAccountName $adAccounts.SuccessfulAccounts
+
+$Catalog = Get-BrokerCatalog -Name $provSchemeName
+
+foreach ($ADAccount in $vms.CreatedVirtualMachines)
+{
+    New-BrokerMachine -MachineName $ADAccount.ADAccountSid -CatalogUid $Catalog.Uid
+    $MachineArray.Add($ADAccount.IdentityPoolName)
+}
+
+Stop-LogHighLevelOperation -HighLevelOperationId $LogOBJ.Id -IsSuccessful $True
+
+for($i=0;$i -lt $machineCount;$i++)
+{
+    $NumDesktops = 1
+    $peakPoolSize = 2
+    $SrcCatalog = $provSchemeName 
+    $weekendPoolSizeByHour = new-object int[] 24
+    $weekdayPoolSizeByHour = new-object int[] 24
+    9..17 | %{ $weekdayPoolSizeByHour[$_] = $peakPoolSize } 
+    $peakHours = (0..23 | %{ $_ -ge 9 -and $_ -le 17 })
+    $logId = Start-LogHighLevelOperation -Text "Create PvD desktop group" `
+        -Source "Create PvD Desktop Group Script" 
+
+    $grp = New-BrokerDesktopGroup  -DesktopKind 'Private'  -DeliveryType 'DesktopsOnly'  -LoggingId $logId.Id -Name $GroupNameArray[$i].ToString() -PublishedName $GroupNameArray[$i].ToString() -SessionSupport 'SingleSession' -ShutdownDesktopsAfterUse $False
+    
+    $grp = Get-BrokerDesktopGroup  -Name $GroupNameArray[$i].ToString()
+
+    $count = Add-BrokerMachinesToDesktopGroup -Catalog $SrcCatalog -Count $NumDesktops -DesktopGroup $GroupNameArray[$i].ToString() -LoggingId $logId.Id
+    
+    New-BrokerAssignmentPolicyRule -DesktopGroupUid $grp.Uid -Enabled $True -IncludedUserFilterEnabled $False -LoggingId $logId.Id -MaxDesktops 1 -Name ($GroupNameArray[$i].ToString() + '_AssignRule') | Out-Null
+    
+    New-BrokerAccessPolicyRule -AllowedConnections 'NotViaAG' -AllowedProtocols @('HDX','RDP') -AllowedUsers 'Filtered' -AllowRestart $True -DesktopGroupUid $grp.Uid -Enabled $True -IncludedSmartAccessFilterEnabled $True -IncludedUserFilterEnabled $True  -IncludedUsers $UserListArray[$i].ToString() -LoggingId $logId.Id -Name ($GroupNameArray[$i].ToString() + '_Direct') | Out-Null
+	
+    New-BrokerAccessPolicyRule  -AllowedConnections 'ViaAG' -AllowedProtocols @('HDX','RDP') -AllowedUsers 'Filtered' -AllowRestart $True -DesktopGroupUid $grp.Uid -Enabled $True -IncludedSmartAccessFilterEnabled $True -IncludedSmartAccessTags @() -IncludedUserFilterEnabled $True -IncludedUsers $UserListArray[$i].ToString() -LoggingId $logId.Id -Name ($GroupNameArray[$i].ToString() + '_AG') | Out-Null
+    
+    New-BrokerPowerTimeScheme -DaysOfWeek 'Weekdays' -DesktopGroupUid $grp.Uid -DisplayName 'Weekdays' -LoggingId $logId.Id -Name ($GroupNameArray[$i].ToString() + '_Weekdays') -PeakHours $peakHours -PoolSize $weekdayPoolSizeByHour | Out-Null 
+    
+    New-BrokerPowerTimeScheme  -DaysOfWeek 'Weekend'  -DesktopGroupUid $grp.Uid -DisplayName 'Weekend' -LoggingId $logId.Id  -Name ($GroupNameArray[$i].ToString() + '_Weekend')  -PeakHours $peakHours -PoolSize $weekendPoolSizeByHour | Out-Null	
+    
+    Stop-LogHighLevelOperation  -HighLevelOperationId $logId.Id -IsSuccessful $True 
+}
+
